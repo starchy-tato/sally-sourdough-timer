216e41a5cf80fbe741b8d5d167d2087d0d3d00b6
Nhu Phan <nb.phan@hotmail.com>
2024-02-05 00:26:14 -0700

    Nhu: add basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Container } from "@mui/material";
+import Grid from "@mui/system/Unstable_Grid";
 
 function App() {
   return (
-    <div className="App">
-      Sally Sourdough Timer!
-    </div>
+    <Container>
+      <Grid container spacing={2}>
+        <Grid>
+          <div>Header</div>
+        </Grid>
+        <Grid>
+          <div>Header</div>
+        </Grid>
+        <Grid>
+          <div>Header</div>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
