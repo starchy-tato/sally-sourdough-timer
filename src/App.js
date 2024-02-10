b0624d5c21797b7e4b8dcb929dbab20ea7d85a18
starchy-tato <79064861+starchy-tato@users.noreply.github.com>
2024-02-10 11:25:54 -0700

    Tiff: Create header component
    * Add component directory

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import { Container } from "@mui/material";
-import Grid from "@mui/system/Unstable_Grid";
+import Header from "./components/Header/Header";
+
 
 function App() {
   return (
-    <Container>
-      <Grid container spacing={2}>
-        <Grid>
-          <div>Header</div>
-        </Grid>
-        <Grid>
-          <div>Header</div>
-        </Grid>
-        <Grid>
-          <div>Header</div>
-        </Grid>
-      </Grid>
-    </Container>
+  <>
+  <Header />
+  </>
   );
 }
 
