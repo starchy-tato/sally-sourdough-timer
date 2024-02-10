a69342b9e95291d103bb51feffc87a94b56bb653
starchy-tato <79064861+starchy-tato@users.noreply.github.com>
2024-02-10 11:55:28 -0700

    Tiff: Updated timer input component
    * Install date picker package
    * Add new component to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import TimerInput from "./components/TimerInput";
 
 function App() {
   return (
   <>
   <Header />
+  <TimerInput/>
   </>
   );
 }
---
+++ src/components/TimerInput/index.js
@@ -0,0 +1,11 @@
+import { TimeField } from '@mui/x-date-pickers/TimeField';
+
+const TimerInput = () => {
+return(
+    <div>
+        Hi this is timer input
+    </div>
+)
+}
+
+export default TimerInput;
