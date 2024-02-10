455e5872598dc2883099439428c89426742890d4
starchy-tato <79064861+starchy-tato@users.noreply.github.com>
2024-02-10 12:08:22 -0700

    Tiff: Use time picker

--- src/components/TimerInput/index.js
+++ src/components/TimerInput/index.js
@@ -1,11 +1,11 @@
-import { TimeField } from '@mui/x-date-pickers/TimeField';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 const TimerInput = () => {
 return(
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <TimeField />
+        <TimePicker label="Start time"/>
     </LocalizationProvider>
 )
 }
