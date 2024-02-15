2e5fc70786fd1d7d89f74640c27dbc6a3e6acc44
starchy-tato <79064861+starchy-tato@users.noreply.github.com>
2024-02-14 22:47:31 -0700

    Add placeholder feed starter step
    * Add date time formatting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import Header from "./components/Header";
 import TimerInput from "./components/TimerInput";
+import { useState } from "react"
 
 function App() {
+  const [feedStarterTime, setFeedStarterTime] = useState(null)
+
+  //@ts-check
+  const handleSetFeedStarterTime = (time) => {
+    const datetime = new Date(time);
+    const formattedTime = datetime.toLocaleTimeString()
+
+
+    console.log(formattedTime);
+
+
+    setFeedStarterTime(formattedTime)
+  }
+
   return (
   <>
   <Header />
-  <TimerInput/>
+  <TimerInput value={feedStarterTime} handleSelectedTime={handleSetFeedStarterTime} />
+  <p>Feed Starter at: {feedStarterTime}</p>
   </>
   );
 }
--- src/components/TimerInput/index.js
+++ src/components/TimerInput/index.js
@@ -2,11 +2,16 @@ import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
-const TimerInput = () => {
+const TimerInput = ({ value, handleSelectedTime }) => {
+    const selectedTime = (time) => {
+        handleSelectedTime(time)
+    }
+
 return(
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <TimePicker label="Start time"/>
+        <TimePicker label="Start Time" value={value} onChange={selectedTime}/>
     </LocalizationProvider>
+  
 )
 }
 
